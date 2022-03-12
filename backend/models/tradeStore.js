a421d44e632fbf1d9d537d47d697b78197ff5eea
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2022-03-12 13:30:19 -0600

    Added tradeStoreSchema

---
+++ backend/models/tradeStore.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const tradeStoreSchema = mongoose.Schema({
+    searchResults: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'TradeItem',
+            default: [],
+        },
+    ],
+});
+
+exports.TradeStore = mongoose.model('TradeStroe', tradeStoreSchema);
