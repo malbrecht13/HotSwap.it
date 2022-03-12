40c5d65612bf56bc959c96f02a1c734fcbc88af4
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2022-03-12 13:21:20 -0600

    Added notificationSchema

---
+++ backend/models/notification.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = mongoose.Schema({
+  type: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  tradeItem: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  }
+});
+
+// The following two functions allow us to use "id" instead of "_id" in queries
+notificationSchema.virtual('id').get(function() {
+  return this._id.toHexString();
+})
+
+notificationSchema.set('toJSON', {
+  virtuals: true
+})
+
+exports.Notification = mongoose.model('Notification', notificationSchema);
