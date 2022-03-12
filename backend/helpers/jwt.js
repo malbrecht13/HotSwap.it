6887de1e2efd90c55f9a3b71a964a4e367912d23
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2022-03-12 14:14:28 -0600

    added authJwt

---
+++ backend/helpers/jwt.js
@@ -0,0 +1,13 @@
+const expressJwt = require('express-jwt');
+
+function authJwt() {
+  const secret = process.env.SECRET;
+  return expressJwt(
+    {
+      secret,
+      algorithms: ['HS256']
+    }
+  )
+}
+
+module.exports = authJwt;
