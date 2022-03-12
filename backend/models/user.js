5ce6471f54eeb49f9e1c254552764ed71f136601
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2022-03-12 12:21:31 -0600

    Added userSchema

---
+++ backend/models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username: {
+      type: String,
+      required: true
+    },
+    passwordHash: {
+      type: String,
+      required: true
+    },
+    loginStatus: {
+      type: String,
+      default: "loggedOut"
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    shippingAddressLine1: {
+      type: String,
+      required: true
+    },
+    shippingAddressLine2: {
+      type: String
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    zip: {
+      type: String,
+      required: true
+    },
+    store: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'UserStore'
+    },
+    notifications: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Notification'
+    }],
+    messages: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Message'
+    }]
+});
+
+// The following two functions allow us to use "id" instead of "_id"
+userSchema.virtual('id').get(function() {
+  return this._id.toHexString();
+})
+
+userSchema.set('toJSON', {
+  virtuals: true
+})
+
+exports.User = mongoose.model('User', userSchema);
