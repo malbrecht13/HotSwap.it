b951d658d6ce17faa98f486fe1eaebfefe84be6e
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2022-03-27 13:05:54 -0500

    Delete user as required from notificationSchema

--- backend/models/notification.js
+++ backend/models/notification.js
@@ -4,7 +4,6 @@ const notificationSchema = mongoose.Schema({
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
-      required: true
     },
      type: {
         type: String,
