e8e788b02e5f389bdc137143ca7911958e23b577
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2022-03-27 14:41:40 -0500

    Image path correct for localhost

--- backend/controllers/tradeItems.js
+++ backend/controllers/tradeItems.js
@@ -64,7 +64,8 @@ const deleteTradeItem = async (req, res) => {
         let image = itemToDelete.image;
         const startOfPath = image.indexOf('/public');
         const imagePath = image.substring(startOfPath);
-        fs.unlinkSync(imagePath);
+        fs.unlinkSync('../backend' + imagePath);
+
 
         const newUserStoreItemsForTrade = userStore.itemsForTrade.filter(
             (tradeItemId) => {
