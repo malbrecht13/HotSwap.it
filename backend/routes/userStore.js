709631965dd915b28621113d9401ae7b27b5376e
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2022-03-27 13:56:16 -0500

    Change path back

--- backend/routes/userStore.js
+++ backend/routes/userStore.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
     if(isValid) {
       uploadError = null;
     }
-    cb(uploadError, 'backend/public/uploads')
+    cb(uploadError, 'public/uploads')
   },
   filename: function (req, file, cb) {
     const fileName = file.originalname.split(' ').join('-');
