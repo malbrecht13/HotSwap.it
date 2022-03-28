71d546a6c18a1c55a4fe4ca2936652b23a6fb9ca
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2022-03-28 18:12:36 -0500

    Update heroku port

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(`${api}/userstore`, userStoreRouter);
 app.use(`${api}/tradeitems`, tradeItemRouter);
 app.use(`${api}/trade`, tradeRouter);
 
-let port;
+let port = process.env.PORT;
 if (!process.env.NODE_ENV === 'production') {
   port = process.env.PORT || 4200;
 }
