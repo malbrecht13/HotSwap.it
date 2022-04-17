80be9973cf813d880b958f563e3c665f8394da78
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2022-04-16 23:50:46 -0500

    Change headers

--- backend/app.js
+++ backend/app.js
@@ -16,9 +16,6 @@ app.use(cors('http://localhost:3000'));
 app.use(function(req,res,next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   next();
 })
 // app.use(authJwt());  //use to require authorization to access api
