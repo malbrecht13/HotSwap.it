01257932570666681b9b794f137070664f01cab7
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2022-04-16 23:57:05 -0500

    Use cors

--- backend/app.js
+++ backend/app.js
@@ -12,13 +12,13 @@ const errorHandler = require('./helpers/error-handler'); // handle api errors
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 // app.use(morgan('tiny'));
-app.use(cors('http://localhost:3000'));
-app.use(function(req,res,next) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  next();
-})
-// app.use(authJwt());  //use to require authorization to access api
+app.use(cors());
+// app.use(function(req,res,next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   next();
+// })
+app.use(authJwt());  //use to require authorization to access api
 app.use(errorHandler);
 app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
 
