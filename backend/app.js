153bfd37dbb95e095a5acfc3d3e9450ea418ef0f
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2022-04-25 08:47:13 -0500

    Don't use authJwt

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 // app.use(morgan('tiny'));
-app.use(authJwt());  //use to require authorization to access api
+// app.use(authJwt());  //use to require authorization to access api
 app.use(errorHandler);
 app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
 
